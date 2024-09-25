91b533d769bbb0b8056dc8be623c12240546f3b4
JackPerry0 <157064817+JackPerry0@users.noreply.github.com>
2024-09-25 00:31:39 -0400

    Implemented the Parser class for this project. This encompassed swifting through the data because some of the values were empty, which lead to errors in other parts of the code, in addition I had to add a "regex", which essentially allowed me to split the lines by commas that were followed by 0 or an even number of commas. In addition I adjusted the iterator method such that it is now in-order rather than pre-order this should make it easier to identify that the code is working. In addition, I also had to make some other adjustments to handle different inputs: in the event that they want to remove or add an object I need to seperate the command and the comma seperated values whereas if the user wants to just print I would only need to check for the command, this required an if-else statement which may appear to be unusual but serves a very imperative purpose. Now one can add insert and print all the companies sorted for current price. NOTE: I am current assuming that the input form is correct in order to receive an appropriate response. The Correct form requires two things: commands are seperated from the values by at least one whitespace. Two, the value must contain all sixteen inputs seperated by commas, and they cannot be empty and they must be of the correct type.

--- src/Parser.java
+++ src/Parser.java
@@ -1,31 +1,192 @@
 import java.io.*;
 import java.util.Scanner;
-
+import java.util.Arrays;
 public class Parser {
 
-    // Create a BST tree of your class type (Note: Replace "Object" with your class type)
-    private BST<Object> mybst = new BST<>();
+    //Create a BST tree of Integer type
+    private BST<SPY> mybst = new BST<>();
 
-    public Parser(String filename) throws FileNotFoundException {
-        process(new File(filename));
+    public Parser(String filename, String csv) throws FileNotFoundException {
+        process(new File(filename), new File(csv));
     }
 
     // Implement the process method
     // Remove redundant spaces for each input command
-    public void process(File input) throws FileNotFoundException {
+    public void process(File input, File csv) throws FileNotFoundException {
 
-        //call operate_BST method;
+        FileInputStream csvStream = new FileInputStream(csv);
+        Scanner csvScan = new Scanner(csvStream);
+
+        String line = csvScan.nextLine();
+
+        while(csvScan.hasNext()){
+
+            line = csvScan.nextLine();
+            line = line.trim();
+
+            if(line.isEmpty()) continue;
+
+            String [] p = csvSplit(line);
+
+            if (p.length != 16 || oneElementIsEmpty(p))
+                continue;
+
+            SPY s = new SPY(p[0],
+                    p[1],
+                    p[2],
+                    p[3],
+                    p[4],
+                    p[5],
+                    Double.parseDouble(p[6]),
+                    Double.parseDouble(p[7]),
+                    Double.parseDouble(p[8]),
+                    Double.parseDouble(p[9]),
+                    p[10],
+                    p[11],
+                    p[12],
+                    p[13],
+                    p[14],
+                    Double.parseDouble(p[15]));
+
+            mybst.insert(s);
+
+
+        }
+
+        csvScan.close();
+
+        FileInputStream Stream = new FileInputStream(input);
+        Scanner sc = new Scanner(Stream);
+
+
+
+        while(sc.hasNext()){
+
+            line = sc.nextLine();
+            line = line.trim();
+
+            if(line.isEmpty()) continue;
+
+            int firstSpaceIndex = line.indexOf(" ");
+
+            if(firstSpaceIndex == -1){
+                operate_BST(line.split("\\s+"));
+            } else {
+
+                String command = line.substring(0, firstSpaceIndex);
+                String vals = line.substring(firstSpaceIndex + 1);
+
+                vals = vals.trim();
+
+                String[] p = csvSplit(vals);
+
+
+                String[] combined = new String[p.length + 1];
+
+                combined[0] = command;
+                int counter = 1;
+                for (String e : p) {
+                    combined[counter] = e;
+                    counter++;
+                }
+
+                //call operate_BST method;
+                operate_BST(combined);
+            }
+
+        }
+
+        sc.close();
+
+    }
+
+    public boolean oneElementIsEmpty(String[] p){
+        for (String e: p) {
+            if (e.isEmpty())
+                return true;
+        }
+        return false;
+    }
+
+    public String[] csvSplit(String line){
+        return line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+    }
+
+    public SPY createObj(String[] p){
+        if (p.length != 17 || oneElementIsEmpty(p)) {
+            System.out.println(Arrays.toString(p));
+            writeToFile("invalid input", "./result.txt");
+            return null;
+        }
+        //create an object
+        SPY obj = new SPY(p[1],
+                p[2],
+                p[3],
+                p[4],
+                p[5],
+                p[6],
+                Double.parseDouble(p[7]),
+                Double.parseDouble(p[8]),
+                Double.parseDouble(p[9]),
+                Double.parseDouble(p[10]),
+                p[11],
+                p[12],
+                p[13],
+                p[14],
+                p[15],
+                Double.parseDouble(p[16])
+        );
+        return obj;
     }
 
     // Implement the operate_BST method
     // Determine the incoming command and operate on the BST
-    public void operate_BST(String[] command) {
-        switch (command[0]) {
-            // add your cases here 
+    public void operate_BST(String[] p) {
+        SPY obj;
+        switch (p[0]) {
+            case "insert":
+                obj = createObj(p);
+                mybst.insert(obj);
+                writeToFile("insert " + obj.toString(),"./result.txt" );
+                break;
+            // add your cases here
             // call writeToFile
+            case "remove":
+                obj = createObj(p);
+                if(mybst.searchRecursive(mybst.getRoot(), obj) == null)
+                    writeToFile("remove failed" ,"./result.txt" );
+                else
+                    writeToFile("removed " + obj.toString(),"./result.txt" );
 
+                mybst.remove(obj);
+                break;
+
+            case "search":
+                obj = createObj(p);
+                if(mybst.searchRecursive(mybst.getRoot(), obj) == null)
+                    writeToFile("Search failed","./result.txt" );
+                else
+                    writeToFile("found " + obj.toString(),"./result.txt" );
+                break;
+
+            case "print":
+                String s = "";
+                for(SPY v : mybst)
+                    s = s + v.toString() + " ";
+
+                writeToFile(s, "./result.txt");
+                break;
+
+            case "isEmpty":
+                if(mybst.isEmpty())
+                    writeToFile("Empty","./result.txt" );
+                else
+                    writeToFile("Not Empty","./result.txt" );
+                break;
             // default case for Invalid Command
-            default -> writeToFile("Invalid Command", "./result.txt");
+            default:
+                writeToFile("Invalid Command", "./result.txt");
+                break;
         }
     }
 
@@ -33,5 +194,14 @@ public class Parser {
     // Generate the result file
     public void writeToFile(String content, String filePath) {
 
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+            writer.write(content);
+            writer.newLine();
+            writer.newLine();
+        } catch(IOException e){
+            System.out.println("error " + e);
+        }
+
     }
+
 }
